2f910648e15d851635070ebeb609bc311fba448b
Valentin <56231226+vsnenashev@users.noreply.github.com>
2024-07-08 18:49:47 +0400

    chapter1.scm

--- chapter1.scm
+++ chapter1.scm
@@ -77,3 +77,33 @@ normal-order evaluation? Explain your answer. |#
 #| Solution: Applicative order of evaluation will result in infinite recursion for 
 (test 0 (p)), whereas in normal order evaluation,
 (test 0 (p)) will return 0 without evaluating (p) |#
+
+
+#|Exercise 1.6: Alyssa P. Hacker doesn’t see why if needs to be provided as
+a special form. “Why can’t I just define it as an ordinary procedure in
+terms of cond?” she asks. Alyssa’s friend Eva Lu Ator claims this can indeed
+be done, and she defines a new version of if:|#
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+;Eva demonstrates the program for Alyssa:
+
+(new-if (= 2 3) 0 5) > 5
+
+(new-if (= 1 1) 0 5) > 0
+
+;Delighted, Alyssa uses new-if to rewrite the square-root program:
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+;What happens when Alyssa aempts to use this to compute square roots? Explain.
+
+#| Solution: Eva's new-if always evaluates both possible answers
+(then-clause and else-clause) regardless of the condition, leading to
+aunnecessary computations and errors in recursive use.|#
