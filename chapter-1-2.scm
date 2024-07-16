881b6dc5fe08939742f8e8221af7daacb9b8184a
Valentin <56231226+vsnenashev@users.noreply.github.com>
2024-07-16 04:03:56 +0400

    Update chapter-1-2.scm

--- chapter-1-2.scm
+++ chapter-1-2.scm
@@ -443,3 +443,39 @@ way to think about the design of iterative algorithms.) |#
 (define (even? n)
   (= (remainder n 2) 0))
 
+
+#|Exercise 1.17: The exponentiation algorithms in this section are
+based on performing exponentiation by means of repeated multiplication.
+In a similar way, one can perform integer multiplication by means of
+repeated addition. The following multiplication procedure (in which
+it is assumed that our language can only add, not multiply) is
+analogous to the expt procedure: |#
+
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+#| This algorithm takes a number of steps that is linear in b. Now
+suppose we include, together with addition, operations double, which
+doubles an integer, and halve, which divides an (even) integer by 2.
+Using these, design a multiplication procedure analogous to fast-expt 
+that uses a logarithmic number of steps.|#
+
+;; Solution:
+(define (fast-mul a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-mul a (halve b))))
+        (else (+ a (fast-mul a (- b 1))))))
+
+(define (double x) (+ x x))
+
+(define (halve x) (/ x 2))
+
+
+#|Exercise 1.18: Using the results of Exercise 1.16 and Exercise 1.17,
+devise a procedure that generates an iterative process for multiplying 
+two integers in terms of adding, doubling, and halving and uses a
+logarithmic number of steps.
+
+;; Solution:
