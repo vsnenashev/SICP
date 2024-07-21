4a77e39c1562e0f1cec8e9a87dd71addbd841da9
Valentin <56231226+vsnenashev@users.noreply.github.com>
2024-07-21 22:08:37 +0400

    Update chapter-1-2.scm

--- chapter-1-2.scm
+++ chapter-1-2.scm
@@ -536,3 +536,190 @@ of steps.|#
                         p
                         q
                         (- count 1)))))
+
+
+#|Exercise 1.20: The process that a procedure generates is
+of course dependent on the rules used by the interpreter.
+As an example, consider the iterative gcd procedure given
+above. Suppose we were to interpret this procedure using
+normal-order evaluation, as discussed in Section 1.1.5. (The
+normal-order-evaluation rule for if is described in Exercise
+1.5.) Using the substitution method (for normal order),
+illustrate the process generated in evaluating (gcd 206 40) and
+indicate the remainder operations that are actually performed.
+How many remainder operations are actually performed in the
+normal-order evaluation of (gcd 206 40)?
+In the applicative-order evaluation?|#
+
+;; Solution:
+;; Normal Order Evaluation
+
+;; When using an interpreter that employs normal order evaluation,
+;; the interpreter will "fully expand and then reduce."
+
+(gcd 206 40) ;; expands to:
+
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+
+;; After evaluating the `if` expression:
+
+(gcd 40 (remainder 206 40)))
+
+;; Which expands to:
+
+(if (= (remainder 206 40) 0)
+    40
+    (gcd (remainder 206 40)
+         (remainder 40 (remainder 206 40))))
+
+;; During the next evaluation of `if`, the `remainder` will be
+;; executed for the 1 time ((remainder 206 40) = 6):
+
+(if (= 6 0)
+    40
+    (gcd (remainder 206 40)
+         (remainder 40 (remainder 206 40))))
+
+;; Then:
+
+(gcd (remainder 206 40)
+     (remainder 40 (remainder 206 40)))
+
+;; Which expands to:
+
+(if (= (remainder 40 (remainder 206 40)) 0)
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))))
+
+;; At this step, evaluating `if` requires computing `remainder`
+;; 2 times ((remainder 40 (remainder 206 40)) = (remainder 40 6) = 4):
+
+(if (= 4 0)
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))))
+
+;; Then:
+
+(gcd (remainder 40 (remainder 206 40))
+     (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40))))
+
+;; Which expands to:
+
+(if (= (remainder (remainder 206 40)
+                  (remainder 40 (remainder 206 40)))
+       0)
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40)))))
+
+;; At this step, evaluating `if` requires computing
+;; `remainder` 4 times ((remainder (remainder 206 40)
+;;                                 (remainder 40 (remainder 206 40))) =
+;;                                                            = … = 2):
+
+(if (= 2 0)
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40)
+                    (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))))
+
+;; Then:
+
+(gcd (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40))
+                (remainder (remainder 206 40)
+                           (remainder 40 (remainder 206 40)))))
+
+;; Which expands to:
+
+(if (= (remainder (remainder 40 (remainder 206 40))
+                  (remainder (remainder 206 40)
+                             (remainder 40 (remainder 206 40))))
+       0)
+    (remainder (remainder 206 40)
+               (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40)
+                                          (remainder 40 (remainder 206 40)))))))
+
+;; At this step, evaluating `if` requires computing `remainder` 7 times:
+
+(if (= 0 0)
+    (remainder (remainder 206 40)
+               (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40))
+                    (remainder (remainder 206 40)
+                               (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 40 (remainder 206 40))
+                               (remainder (remainder 206 40)
+                                          (remainder 40 (remainder 206 40)))))))
+
+;; To get the final result, `remainder` needs to be
+;; computed 4 additional times:
+
+(remainder (remainder 206 40)
+           (remainder 40 (remainder 206 40)))
+
+2
+
+;; Using normal-order evaluation, `remainder`
+;; is called 1 + 2 + 4 + 7 + 4 = 18 times.
+
+
+;; Applicative-order evaluation
+
+;; An interpreter that uses applicative-order evaluation will
+;; “evaluate the arguments and then apply”.
+
+(gcd 206 40)
+
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+
+(gcd 40 6)
+
+(if (= 6 0)
+    40
+    (gcd 6 (remainder 40 6)))
+
+(gcd 6 4)
+
+(if (= 4 0)
+    6
+    (gcd 4 (remainder 6 4)))
+
+(gcd 4 2)
+
+(if (= 2 0)
+    4
+    (gcd 2 (remainder 4 2)))
+
+(gcd 2 0)
+
+(if (= 0 0)
+    2
+    (gcd 0 (remainder 2 0)))
+
+2
+
+;; Using applicative-order evaluation, `remainder`
+;; is called 4 times.
+
+
