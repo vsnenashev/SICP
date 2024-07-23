524ed9ae05914363d7b03918b32d91c8dda76d97
Valentin <56231226+vsnenashev@users.noreply.github.com>
2024-07-23 15:51:58 +0400

    Update chapter-1-2.scm

--- chapter-1-2.scm
+++ chapter-1-2.scm
@@ -723,3 +723,30 @@ In the applicative-order evaluation?|#
 ;; is called 4 times.
 
 
+#|Exercise 1.21: Use the smallest-divisor procedure to find
+the smallest divisor of each of the following numbers: 199, 1999, 19999.|#
+
+;; Solution:
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+;; > 199
+;; this number is prime
+
+(smallest-divisor 1999)
+;; > 1999
+;; this number is prime
+
+(smallest-divisor 19999)
+;; > 7
+;; this number is not prime
+;; 19999 = 7*2857
